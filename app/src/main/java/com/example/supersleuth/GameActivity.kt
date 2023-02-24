28cf293edce18dbbf576f02b4dcca8da9d729690
brhinman20 <brhinman20@my.trine.edu>
2023-02-24 09:43:19 -0500

    Added transitions

--- app/src/main/java/com/example/supersleuth/GameActivity.kt
+++ app/src/main/java/com/example/supersleuth/GameActivity.kt
@@ -1,11 +1,32 @@
 package com.example.supersleuth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class GameActivity : AppCompatActivity() {
+    private var gameScore: Int = 0
+    private var highScoreVal: Int = 0
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        val highScore =intent.getStringExtra("highscore")
+        if (highScore != null) {
+            highScoreVal = highScore.toInt()
+        }
+    }
+
+    fun concedeTime(view: View) {
+        // Do something in response to button click
+        if(gameScore > highScoreVal) {
+            highScoreVal = gameScore
+        }
+        val intent = Intent(this, ScoreActivity::class.java)
+        intent.putExtra("score", gameScore.toString())
+        intent.putExtra("highscore", highScoreVal.toString())
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/example/supersleuth/MainActivity.kt
+++ app/src/main/java/com/example/supersleuth/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.supersleuth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+    private var highScore: String = "0"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        highScore= intent.getStringExtra("highscore")!!
+    }
+
+    fun playTime(view: View) {
+        // Do something in response to button click
+        val intent = Intent(this, ScoreActivity::class.java)
+        intent.putExtra("highscore", highScore)
+        startActivity(intent)
+        finish()
+    }
+
+    fun quitTime(view: View) {
+        // Do something in response to button click
+        exitProcess(0)
     }
 }
