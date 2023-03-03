673c22e368ddf5834058b405f0e5475e3023dfaa
brhinman20 <brhinman20@my.trine.edu>
2023-03-03 09:51:39 -0500

    Added Start of Funcs

--- app/src/main/java/com/example/supersleuth/GameActivity.kt
+++ app/src/main/java/com/example/supersleuth/GameActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import java.lang.Long.parseLong
 
 class GameActivity : AppCompatActivity() {
     private var gameScore: Int = 0
@@ -25,14 +26,28 @@ class GameActivity : AppCompatActivity() {
     Teal 	#008080 	rgb(0, 128, 128)
     Blue 	#0000FF 	rgb(0, 0, 255)
     Navy 	#000080 	rgb(0, 0, 128)
-    Fuchsia 	#FF00FF 	rgb(255, 0, 255)
+    Pink	#FF00FF 	rgb(255, 0, 255)
     Purple 	#800080 	rgb(128, 0, 128)
     */
 
     private val colorValWhite: IntArray = intArrayOf(255, 255, 255)
     private val colorValSilver: IntArray = intArrayOf(192, 192, 192)
+    private val colorValGray: IntArray = intArrayOf(128, 128, 128)
+    private val colorValBlack: IntArray = intArrayOf(0, 0, 0)
+    private val colorValRed: IntArray = intArrayOf(255, 0, 0)
+    private val colorValMaroon: IntArray = intArrayOf(128, 0, 0)
+    private val colorValYellow: IntArray = intArrayOf(255, 255, 0)
+    private val colorValOlive: IntArray = intArrayOf(128, 128, 0)
+    private val colorValLime: IntArray = intArrayOf(0, 255, 0)
+    private val colorValGreen: IntArray = intArrayOf(0, 128, 0)
+    private val colorValAqua: IntArray = intArrayOf(0, 255, 255)
+    private val colorValTeal: IntArray = intArrayOf(0, 128, 128)
+    private val colorValBlue: IntArray = intArrayOf(0, 0, 255)
+    private val colorValNavy: IntArray = intArrayOf(0, 0, 128)
+    private val colorValPink: IntArray = intArrayOf(255, 0, 255)
+    private val colorValPurple: IntArray = intArrayOf(128, 0, 128)
 
-
+    private val colorsArray: Array<IntArray> = arrayOf(colorValWhite, colorValSilver, colorValGray, colorValBlack, colorValRed, colorValMaroon, colorValYellow, colorValOlive, colorValLime, colorValGreen, colorValAqua, colorValTeal, colorValBlue, colorValNavy, colorValPink, colorValPurple)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,7 +71,15 @@ class GameActivity : AppCompatActivity() {
         finish()
     }
 
-    fun newColor(oldColor: String) {
+    fun colorCheck(rVal: Int, gVal: Int, bVal: Int) {
+        parseLong(sleuthSearchColor.substring(0, 1), 16)
 
+    }
+
+    fun newColor(oldColor: String) {
+        var newColorVals: String = ""
+        newColorVals += Integer.toHexString(255)
+        newColorVals += Integer.toHexString(0)
+        newColorVals += Integer.toHexString(0)
     }
 }
